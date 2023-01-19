aa11912fc0a1d7be7685a1d2746992586d552830
Jackson Brazeau <braz0198@algonquinlive.com>
2023-01-19 13:47:06 -0500

    hello

---
+++ main.js
@@ -0,0 +1,18 @@
+const firstName = "Jack ' ' "
+
+const lastName = 'Brazeau'
+console.log(firstName + lastName)
+
+const pet= 'dogs' //Template Literal
+
+const number= "2"
+
+console.log(`I have ${number} ${pet}.`)
+
+let points = 5
+let bullets = -3.5
+console.log(points + bullets)
+
+let isAdmin = true //Boolean
+isAdmin = false
+console.log(isAdmin)
